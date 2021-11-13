a43d5b8fea1233f0a8d0f13b95fa69fe091362ad
Tomjosetj31 <tomjosethomastj31@gmail.com>
2021-11-13 13:14:38 +0530

    addd header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi Tom Jose</h1>
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 import { Avatar } from '@material-ui/core'
 import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SearchIcon from '@material-ui/icons/Search'
+import HelpIcon from '@material-ui/icons/Help'
 
 function Header() {
     return (
@@ -11,10 +13,11 @@ function Header() {
                 <AccessTimeIcon />
             </div>
             <div className="header__search">
-
+                <SearchIcon />
+                <input placeholder="search"></input>
             </div>
             <div className="header__right">
-
+                <HelpIcon />
             </div>
         </div>
     )
